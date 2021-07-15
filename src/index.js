ab77273248eb6de1c5478de8f76031db138fac2e
bonnv79 <ngovanbon16@gmail.com>
2021-07-15 12:35:05 +0700

    Remove Home in pages

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import * as utils from './utils';
-import Home from './pages/Home';
 import ChipsArray from './components/ChipsArray';
 
 export default {
   // components
   ChipsArray,
-
-  // pages
-  Home,
 
   // other
   utils,
